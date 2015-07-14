1b5318908994c67d288eca466d9576ba8accf09f
Blake Elshire <belshire@fossil.com>
2015-07-14 14:43:26 -0500

    Updating storyboard and class for entering location.

--- On The Map/SetLocationViewController.swift
+++ On The Map/SetLocationViewController.swift
@@ -8,7 +8,52 @@
 
 import Foundation
 import UIKit
+import MapKit
 
-class SetLocationViewController: UIViewController {
+class SetLocationViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var locationTextView: UITextView!
+    @IBOutlet weak var linkTextView: UITextView!
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var enterLocationView: UIView!
+    @IBOutlet weak var locationView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
     
+    override func viewDidLoad() {
+        locationView.hidden = true
+        enterLocationView.hidden = false
+        
+        locationTextView.delegate = self
+        linkTextView.delegate = self
+    }
+    
+    @IBAction func cancelModalView(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func findOnMap(sender: AnyObject) {
+        CLGeocoder().geocodeAddressString(locationTextView.text!) { placemarks, error in
+            println(placemarks)
+            if let places = placemarks {
+                var topResult: CLPlacemark = places[0] as! CLPlacemark
+                var placemark: MKPlacemark = MKPlacemark(placemark: topResult)
+                
+                let regionRadius: CLLocationDistance = 1000
+                let coordinateRegion = MKCoordinateRegionMakeWithDistance(placemark.location.coordinate,
+                    regionRadius * 2.0, regionRadius * 2.0)
+                self.mapView.setRegion(coordinateRegion, animated: true)
+                self.mapView.addAnnotation(placemark)
+            }
+        }
+        
+        locationView.hidden = false
+        enterLocationView.hidden = true
+    }
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        textView.text = ""
+    }
+    
+    @IBAction func setLocation(sender: AnyObject) {
+        
+    }
 }
